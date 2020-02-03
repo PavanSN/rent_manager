e01f32dd10bd2a1a153edb8b365e847106a6b65c
Pavansn2000 <pavansn.2000@gmail.com>
2020-02-03 22:45:31 +0530

    v 0.0.3

--- lib/Models/TabPressed.dart
+++ lib/Models/TabPressed.dart
@@ -1,10 +1,15 @@
 import 'package:states_rebuilder/states_rebuilder.dart';
 
-class TenantYearPressed extends StatesRebuilder {
+class TabPressed extends StatesRebuilder {
   int yearPressed;
+  int building;
 
   yearTapped(year) {
     yearPressed = year;
     rebuildStates();
+  }
+
+  buildingTapped(index){
+    building = index;
   }
 }
