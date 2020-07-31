43f28ef68b3f12712208c05cd1f5af216241bb09
Pavan <pavansn.2000@gmail.com>
2020-07-31 10:39:54 +0530

    v2.0.1+19 Stable

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'package:bot_toast/bot_toast.dart';
 import 'package:bubble_bottom_bar/bubble_bottom_bar.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -7,7 +8,6 @@ import 'package:home_manager/CommonFiles/Settings.dart';
 import 'package:home_manager/Models/TabPressed.dart';
 import 'package:home_manager/Tenant/TenantHomePage.dart';
 import 'package:states_rebuilder/states_rebuilder.dart';
-
 import 'CommonFiles/CommonWidgetsAndData.dart';
 import 'CommonFiles/GoogleSignInPage.dart';
 import 'CommonFiles/ProfileScreen.dart';
